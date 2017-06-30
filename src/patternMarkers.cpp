ecff3b7aa87dfc4a0dfec191bb3524e8bd627d3c
Kipper Fletez-Brant <cafletezbrant@gmail.com>
2017-06-30 16:48:16 -0400

    2nd!

--- src/patternMarkers.cpp
+++ src/patternMarkers.cpp
@@ -27,4 +27,35 @@ using namespace Rcpp;
 
 boost::mt19937 rng(43);
 
+// Amatrix: genes x pattern_weights
+// Pmatrix: patterns x samples
+// threshold: "unique" - we'll just use this
+// lp: don't want
+// full: don't want
+// return: partition of genes into list
+
+// [[Rcpp::export()]]
+double max(NumericVector X){
+  double max_val = -std::numeric_limits<double>::infinity();
+  for (int ii=0; ii < X.size(); ii++){
+    if (max_val > X[ii]){ max_val = X[ii]; }
+  }
+  max_val;
+}
+
+// [[Rcpp::export()]]
+List patternMarkers(NumericMatrix A, NumericMatrix P){
+  // get scaling factors for A matrix
+  NumericVector p_scales(P.nrow());
+  for (ii=0; ii < P.nrow(); ii++){
+    p_scales[ii] = max(P(ii, _));
+  }
+  // scale A matrix, assuming P matrix is not scaled
+  for (int ii=0; ii < A.ncol(); ii++){
+    for (int jj=0; jj < A.nrow(); jj++){
+      A(ii, jj) = A(ii, jj) * p_scales[ii];
+    }
+  }
+
+}
 
